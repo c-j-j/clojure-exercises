5d4d316ea83c01a392323bf1a72b1a5e037034d1
Chris Jordan <cjordan@8thlight.com>
2016-08-05 15:39:14 +0100

    Adds member? and substitute.

--- src/clojure_exercises/linked_list.clj
+++ src/clojure_exercises/linked_list.clj
@@ -19,3 +19,18 @@
   (cond (empty? list) '()
         (= (first list) old) (cons new (cons old (insert-multi-to-left-of (rest list) old new)))
         :else (cons (first list) (insert-multi-to-left-of (rest list) old new))))
+
+(defn replace-first-element [list old new]
+  (cond (empty? list) '()
+        (= (first list) old) (cons new (rest list))
+        :else (cons (first list) (replace-first-element (rest list) old new))))
+
+(defn replace-all-elements [list old new]
+  (cond (empty? list) '()
+        (= (first list) old) (cons new (replace-all-elements (rest list) old new))
+        :else (cons (first list) (replace-all-elements (rest list) old new))))
+
+(defn member? [list a]
+  (cond (empty? list) false
+        (= a (first list)) true
+        :else (member? (rest list) a)))
--- test/clojure_exercises/linked_list_test.clj
+++ test/clojure_exercises/linked_list_test.clj
@@ -14,7 +14,16 @@
     (is (= (insert-multi-to-right-of '(1 2 3 2 5) 2 :b) '(1 2 :b 3 2 :b 5))))
   (testing "find all elements and insert to the left of it"
     (is (= (insert-multi-to-left-of '() 0 1) '()))
-    (is (= (insert-multi-to-left-of '(1 2 3 2 5) 2 :b) '(1 :b 2 3 :b 2 5)))))
+    (is (= (insert-multi-to-left-of '(1 2 3 2 5) 2 :b) '(1 :b 2 3 :b 2 5))))
+  (testing "substitute one element"
+    (is (= (replace-first-element '() 0 1) '()))
+    (is (= (replace-first-element '(1 2 3) 3 :x) '(1 2 :x))))
+  (testing "substitute all elements"
+    (is (= (replace-all-elements '() 0 1) '()))
+    (is (= (replace-all-elements '(1 2 3 2 3) 3 :x) '(1 2 :x 2 :x))))
+  (testing "member?"
+    (is (= (member? '() :a) false))
+    (is (= (member? '(:b :c :a) :a) true))))
 
 
 
